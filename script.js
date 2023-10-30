651702317439a01f0347f75616b8b5f2c441a726
balazsbatorszigetvary <balazs.bator.szigetvary@gmail.com>
2023-10-30 09:51:31 +0100

    mikmak-studio-fullpage.js

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+new fullpage('#fullpage', {
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor : ['rgb(65, 172, 214)', 'rgb(254, 101, 87)','rgb(219, 75, 134)','rgb(0, 212, 216)','rgb(112, 86, 175)','rgb(219, 75, 134)'],
+    navigation: true,
+    anchors: ['section1', 'section2', 'section3','section4','section5'],
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
